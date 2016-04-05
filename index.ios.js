0599a9d503fdd3db298b1bd7f68eb5088833700a
Daw-Chih Liou <dawchih.liou@bloomon.nl>
2016-04-05 10:38:07 +0800

    add navigatorIOS and scenes

--- index.ios.js
+++ index.ios.js
@@ -6,29 +6,95 @@
 import React, {
   AppRegistry,
   Component,
+  NavigatorIOS,
+  TouchableWithoutFeedback,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
-class Curensea extends Component {
-  render() {
+class First extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  _handleNav () {
+    console.log('props %o', this.props);
+    this.props.navigator.push({
+      title: 'Second',
+      component: Second,
+      passProp: {
+        data: 'dummy from First'
+      }
+    });
+  }
+
+  render () {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Text style={styles.welcome}>First Scene</Text>
+        <TouchableWithoutFeedback onPress={this._handleNav.bind(this)}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>
+              Go To Second Scene
+            </Text>
+          </View>
+        </TouchableWithoutFeedback>
       </View>
+    )
+  }
+}
+
+class Second extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  _handleNav () {
+    this.props.navigator.push({
+      title: 'First',
+      component: First,
+      passProp: {
+        data: 'dummy from Scond'
+      }
+    });
+  }
+
+  render () {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Second Scene</Text>
+        <TouchableWithoutFeedback onPress={this._handleNav.bind(this)}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>
+              Go To First Scene
+            </Text>
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
+    )
+  }
+}
+
+class Curensea extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.nav}
+        itemWrapperStyle={styles.navWrap}
+        initialRoute={{
+          title: 'First Scene',
+          component: First,
+        }}
+      />
     );
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -37,16 +103,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+  nav: {
+    flex: 1,
+  },
+  navWrap: {
+    flex: 1,
+    marginTop: 70,
+  },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+    marginTop: 80,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  button: {
+    backgroundColor: '#009DDD',
+    padding: 10,
+    margin: 10,
   },
+  buttonText: {
+    color: '#fff',
+  }
 });
 
 AppRegistry.registerComponent('Curensea', () => Curensea);
